f069c3cb22588e0a399a85311dc1d3cf1657461b
Abhishek Shah <abhishekshah3@gmail.com>
2025-09-12 23:58:32 +0530

    use: resolve zigversion to semver

--- src/cli/use.rs
+++ src/cli/use.rs
@@ -1,26 +1,55 @@
-use crate::ZigVersion;
+use crate::{Result, app::App};
+use crate::{ZigVersion, tools};
+use color_eyre::eyre::{Result, eyre};
+use semver::Version as SemverVersion;
 use std::path::PathBuf;
 use yansi::Paint;
 
-use crate::{Result, app::App};
-
 /// Main entry point for the use command
-pub(crate) async fn use_version(version: ZigVersion, app: &mut App) -> Result<()> {
-    // First check that if version is a valid version
-    let resolved_version = if version.is_placeholder_version() {
-
-    }
-    println!(
-        "{} {:#?}",
-        Paint::blue("Using Zig version:").bold(),
-        version
-    );
-    let set_zig_version = app.set_active_version(version).await?;
-    println!(
-        "{} {:#?}",
-        Paint::green("✓ Set Zig version to:").bold(),
-        set_zig_version
-    );
+pub(crate) async fn use_version(zig_version: ZigVersion, app: &mut App) -> Result<()> {
+    let placeholder_version = SemverVersion::new(0, 0, 0);
+    // First check that if version is a valid version & resolve it to a Semver i.e. ZigVersion::Semver
+    let using_version: ZigVersion = match zig_version {
+        ZigVersion::Semver(ref v) => {
+            if *v == placeholder_version {
+                return Err(eyre!("Invalid semver version: {}", v));
+            } else {
+                ZigVersion::Semver(*v)
+            }
+        }
+        ZigVersion::Master(ref v) => {
+            assert!(
+                *v == placeholder_version,
+                "Impossible to construct Master with non-placeholder version"
+            );
+            app.fetch_master_version().await?
+        }
+        ZigVersion::Stable(ref v) => {
+            if *v == placeholder_version {
+                app.fetch_stable_version().await?
+            } else {
+                ZigVersion::Semver(*v)
+            }
+        }
+        ZigVersion::Latest(ref v) => {
+            assert!(
+                *v == placeholder_version,
+                "Impossible to construct Latest with non-placeholder version"
+            );
+            app.fetch_latest_version().await?
+        }
+    };
+    // println!(
+    //     "{} {:#?}",
+    //     Paint::blue("Using Zig version:").bold(),
+    //     version
+    // );
+    // let set_zig_version = app.set_active_version(version).await?;
+    // println!(
+    //     "{} {:#?}",
+    //     Paint::green("✓ Set Zig version to:").bold(),
+    //     set_zig_version
+    // );
     // todo!(
     //     "impl use for system, system@<version>, system@<version> --path=<path>, --path=<path>, latest, master, stable, <version>"
     // );
